3e0af159db9be30dffd5ce0ac1cd9f0df2e904db
Matthew Chastain <matthewdchastain@gmail.com>
2015-09-22 17:02:37 -0400

    Added charts for hours analysis

--- scripts/views/analysis-item.js
+++ scripts/views/analysis-item.js
@@ -27,13 +27,107 @@ define([
 				this.$('.fa-angle-double-up').toggle();
 			},
 			hours: function() {
-				console.log('hours');
+				var self = this;
+				this.$('.chart-1').highcharts({
+					chart: {
+			            plotBackgroundColor: null,
+			            plotBorderWidth: null,
+			            plotShadow: false,
+			            type: 'pie'
+			        },
+			        title: {
+			            text: 'Hours Logged vs Hours Remaining'
+			        },
+			        tooltip: {
+			            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+			        },
+			        plotOptions: {
+			            pie: {
+			                allowPointSelect: true,
+			                cursor: 'pointer',
+			                dataLabels: {
+			                    enabled: true,
+			                    format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+			                    style: {
+			                        color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+			                    }
+			                }
+			            }
+			        },
+			        series: [{
+			            name: " ",
+			            colorByPoint: true,
+			            data: [{
+			                name: "Hours Logged",
+			                y: self.checkTime()
+			            }, 
+			            {
+			                name: "Hours Remaining",
+			                y: (40 - self.checkTime()),
+			                sliced: true,
+			                selected: true
+			            }]
+			        }]
+				})
+				this.$('.chart-2').highcharts({
+					chart: {
+			            type: 'column'
+			        },
+			        title: {
+			            text: 'Weekly Hour Totals'
+			        },
+			        xAxis: {
+			            categories: [
+			                '9/3-9/7',
+			                '9/10-9/14',
+			                '9/17-9/21',
+			                '9/24-9/28'
+			            ],
+			            crosshair: true
+			        },
+			        yAxis: {
+			            min: 0,
+			            title: {
+			                text: 'Hours (hr)'
+			            }
+			        },
+			        tooltip: {
+			            headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+			            pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+			                '<td style="padding:0"><b>{point.y:.1f} hr</b></td></tr>',
+			            footerFormat: '</table>',
+			            shared: true,
+			            useHTML: true
+			        },
+			        plotOptions: {
+			            column: {
+			                pointPadding: 0.2,
+			                borderWidth: 0
+			            }
+			        },
+			        series: [{
+			        	name: "Projected Hours",
+			        	data: [45, 45, 45, 45]
+			        }, 
+			        {
+			        	name: "Required Hours",
+			        	data: [40,40,40,40]
+			        }, 
+			        {
+			        	name: "Logged Hours",
+			            data: [50, 71.5, 60, 47]
+
+			        }]
+				})
 			},
 			pto: function() {
 				console.log('pto');
 			},
 			tasks: function() {
 				console.log('tasks');
+			},
+			checkTime: function() {
+				return 30;
 			}
 		})
 	})
